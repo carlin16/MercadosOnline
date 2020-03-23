e7ce6be6b0a0b8c8025ab52272c8c34757d6ce5c
Jefferson Alvarado <jefferson.alvaradome@ug.edu.ec>
2020-03-22 22:11:03 -0500

    Creacion Layout Registrar 23

--- app/src/main/java/com/example/tiendaclient/utils/Global.java
+++ app/src/main/java/com/example/tiendaclient/utils/Global.java
@@ -1,7 +1,10 @@
 package com.example.tiendaclient.utils;
 
+import android.text.TextUtils;
+
 import com.example.tiendaclient.models.recibido.ResponseLoginUser;
 import com.example.tiendaclient.models.recibido.ResponseRegistroUser;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -11,6 +14,7 @@ public class Global {
     public static ResponseLoginUser LoginU= new ResponseLoginUser();
 
 
+
     public static String convertObjToString(Object clsObj) {
         //convert object  to string json
         String jsonSender = new Gson().toJson(clsObj, new TypeToken<Object>() {
@@ -18,7 +22,18 @@ public class Global {
         return jsonSender;
     }
 
+    private Boolean verificar_vacio(String texto) {
 
+
+        //Todo retorno true si esta vacio
+        if (TextUtils.isEmpty(texto)) {
+
+            return true;
+        }
+
+
+        return false;
+    }
 
 
 
--- app/src/main/java/com/example/tiendaclient/view/fragments/registro_datos_usuario.java
+++ app/src/main/java/com/example/tiendaclient/view/fragments/registro_datos_usuario.java
@@ -1,14 +1,27 @@
 package com.example.tiendaclient.view.fragments;
 
+import android.app.Dialog;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.tiendaclient.R;
+import com.google.android.material.textfield.TextInputLayout;
+import com.makeramen.roundedimageview.RoundedImageView;
+import com.rilixtech.widget.countrycodepicker.CountryCodePicker;
+
+import br.com.simplepass.loadingbutton.customViews.CircularProgressButton;
+import in.anshul.libray.PasswordEditText;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,13 +30,76 @@ public class registro_datos_usuario extends Fragment {
 
     public registro_datos_usuario() {
         // Required empty public constructor
+
+
     }
 
+   View vista;
+    EditText Nombres, Apellidos, Direccion, Telefono,  Email,Usuario;
+    TextInputLayout TINom, TIApell, TIDir, TITel, TIEma, TIUsua, TIPassw, TIRePass ;
+    Dialog myDialog;
+    PasswordEditText Pass, RePass;
+    String[] Roles;
+    int posicionRol=0;
+    Boolean cambio=false;
+    String mensaje="";
+    CountryCodePicker codigo_pais;
+    CircularProgressButton BtnRegistrar ,datos,fotos,credenciales;
+
+    String numeroTelefono;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registro_datos_usuario, container, false);
+        vista= inflater.inflate(R.layout.fragment_registro_datos_usuario, container, false);
+        return vista;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+       // vista.findViewById();
+        UI();
+        CLick();
+    }
+
+    public void UI(){
+        Usuario=vista.findViewById(R.id.registro_usuario);
+        Nombres=vista.findViewById(R.id.registro_nombre);
+        Apellidos=vista.findViewById(R.id.registro_apellido);
+        Telefono=vista.findViewById(R.id.telefono_registro);
+        Email=vista.findViewById(R.id.registro_email);
+        Pass=vista.findViewById(R.id.registro_pass);
+        RePass=vista.findViewById(R.id.registro_repass);
+        BtnRegistrar=vista.findViewById(R.id.btn_registro);
+        codigo_pais = vista.findViewById(R.id.ccp);
+        ///
+        Nombres.requestFocus();
+        ///
+
+        TINom=vista.findViewById(R.id.TINombre);
+        TIApell=vista.findViewById(R.id.TIApellido);
+        TIEma=vista.findViewById(R.id.TIEmail);
+        TITel=vista.findViewById(R.id.TITelefono);
+        TIUsua=vista.findViewById(R.id.TIUsuario);
+        TIPassw=vista.findViewById(R.id.TIPass);
+        TIRePass=vista.findViewById(R.id.TIRepass);
+
+    }
+
+    private void CLick(){
+        Nombres.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    TINom.setDefaultHintTextColor(ColorStateList.valueOf(Color.parseColor("#EE8813")));
+                } else {
+                    TINom.setDefaultHintTextColor(ColorStateList.valueOf(Color.parseColor("#CCCCCC")));
+                }
+            }
+        });
+
+    }
+
 }
