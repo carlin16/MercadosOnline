f3e955ef6298fe2fbefa190ef62a609e8a9d3ee9
carloscaguana <carlos.aforever@gmail.com>
2020-03-27 20:00:46 -0500

    Vista de mapas incompleta

--- app/src/main/java/com/example/tiendaclient/view/fragments/ubica_entrega.java
+++ app/src/main/java/com/example/tiendaclient/view/fragments/ubica_entrega.java
@@ -2,6 +2,8 @@ package com.example.tiendaclient.view.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,21 +11,53 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.tiendaclient.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ubica_entrega extends Fragment {
+public class ubica_entrega extends Fragment implements OnMapReadyCallback {
+
+
+    private GoogleMap mMap;
 
     public ubica_entrega() {
         // Required empty public constructor
     }
 
 
+        View vista;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ubica_entrega, container, false);
+        vista= inflater.inflate(R.layout.fragment_ubica_entrega, container, false);
+        return vista;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.Maps);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+        LatLng UCA = new LatLng(-2.176741, -79.828183);
+        mMap.addMarker(new MarkerOptions().position(UCA).title("YOUR TITLE")).showInfoWindow();
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(UCA,17));
+    }
+
+
+
 }
