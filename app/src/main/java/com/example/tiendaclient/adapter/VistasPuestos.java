2129a2665f39a9308ea072625363437384ceb6ad
carloscaguana <carlos.aforever@gmail.com>
2020-05-04 21:57:47 -0500

    prueba 1

--- app/src/main/java/com/example/tiendaclient/adapter/VistasPuestos.java
+++ app/src/main/java/com/example/tiendaclient/adapter/VistasPuestos.java
@@ -118,7 +118,7 @@ public class VistasPuestos extends RecyclerView.Adapter<VistasPuestos.MultiHolde
 });
 
     }
-
+///
     @Override
     public int getItemCount() {
         return lst_normal.size();
